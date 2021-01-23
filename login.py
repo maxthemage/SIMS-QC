0742092ba0a7845649f368943cd9717efc9f2086
maxthemaster <jjmaxy14@gmail.com>
2021-01-23 11:40:57 -0400

    Update login.py with login and sign up logic

--- login.py
+++ login.py
@@ -3,27 +3,48 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk, Image
-
-
-def onClick():
-    password_con = Label(root, text="Confirm Password", font=("Arial", 14))
-    confirm_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12))    
-
-
-
+  
+        
 #root configurations
 root = Tk()
 root.title("SIMS-QC")
 root.iconbitmap("images/logo.ico")
-root.geometry("360x480")
+root.geometry("375x540")
 root.config(bg="#ffffff")
 root.resizable(width=False, height=False)
 
 
+#signup button onclick function
+def onClick():
+    padding_four.grid(column=3, row=10)
+    password_con.grid(column=3, row=11)
+    confirm_ent.grid(column=3, row=12)
+    log_in.config(text="Sign Up")
+    padding_six.destroy()
+    sign_up.destroy()
+    padding_one.config(padx=42)
+  
+
+#submut button function
+def submit():
+        state = log_in.cget("text")
+        #print(state)
+        if state == "Log In":
+            teacherid = teacherid_ent.get() 
+            password = password_ent.get()
+        else:
+            teacherid = teacherid_ent.get()
+            password = password_ent.get()
+            confirmed_password = confirm_ent.get()
+            if password != confirmed_password:
+                messagebox.showerror("Error", "Passwords do not match")
+            else:
+                pass
+
+
 #UI widgets in order of display
-sign_up = Button(root, text="Sign Up", bg="#ffffff", command=onClick)
-sims = Label(root, text="SIMS-QC", bg="#ffffff", font=("Times New Roman", 24))
-padding_one = Label(root, text="", padx=40, bg="#ffffff")
+sims = Label(root, text="SIMS-QC", bg="#ffffff", font=("Times New Roman", 25), pady=5)
+padding_one = Label(root, text="", padx=37, bg="#ffffff")
 account_icon = ImageTk.PhotoImage(Image.open("images/account.jpg"))
 account_img = Label(image=account_icon)
 log_in = Label(root, text="Log In", bg="#ffffff", pady=5, font=("Arial", 25))
@@ -33,8 +54,14 @@ teacherid_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12)
 padding_three = Label(root, text="", bg="#ffffff")
 pass_word = Label(root, text="Password", bg="#ffffff", font=("Arial", 14))
 password_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12))
-padding_four = Label(root, text="", bg="#ffffff", pady=10)
-submit_btn = Button(root, text="Submit", font=("Arial", 13), bg="#138808", fg="#ffffff", width=7)
+padding_four = Label(root, text="", padx=5, bg="#ffffff")
+password_con = Label(root, text="Confirm Password", font=("Arial", 14), bg="#ffffff")
+confirm_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12))
+padding_five = Label(root, text="", bg="#ffffff", pady=10)
+submit_btn = Button(root, text="Submit", font=("Arial", 13), bg="#138808", fg="#ffffff", width=7, command=submit)
+padding_six = Label(root, text="Don't have an account yet? Then sign up.", bg="#ffffff", pady=15)
+sign_up = Button(root, text="Sign Up", fg="#ffffff", bg="#1338be", command=onClick)
+
 
 
 #widgets being displayed using the grid method
@@ -48,9 +75,10 @@ teacherid_ent.grid(column=3, row=6)
 padding_three.grid(column=3, row=7)
 pass_word.grid(column=3, row=8)
 password_ent.grid(column=3, row=9)
-padding_four.grid(column=3, row=10)
-submit_btn.grid(column=3, row=11)
-
+padding_five.grid(column=3, row=13)
+submit_btn.grid(column=3, row=14)
+padding_six.grid(column=3, row=15)
+sign_up.grid(column=3, row=16)
 
 
 root.mainloop()
