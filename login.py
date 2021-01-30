e546bf06608c3b2ec2ae84b935288948b3ccf032
maxthemaster <jjmaxy14@gmail.com>
2021-01-30 00:09:35 -0400

    Add copy of sims-qc.py, sims-qc-copy with older code.

--- login.py
+++ login.py
@@ -16,19 +16,16 @@ root.resizable(width=False, height=False)
 
 #signup button onclick function
 def onClick():
-    padding_four.grid(column=3, row=10)
-    password_con.grid(column=3, row=11)
-    confirm_ent.grid(column=3, row=12)
+    password_con.grid(column=1, row=8, pady=(15,0))
+    confirm_ent.grid(column=1, row=9)
+    submit_btn.grid(column=1, row=10, pady=(25,25))
     log_in.config(text="Sign Up")
-    padding_six.destroy()
+    sign_up_text.destroy()
     sign_up.destroy()
-    padding_one.config(padx=42)
-  
 
 #submut button function
 def submit():
         state = log_in.cget("text")
-        #print(state)
         if state == "Log In":
             teacherid = teacherid_ent.get() 
             password = password_ent.get()
@@ -43,42 +40,33 @@ def submit():
 
 
 #UI widgets in order of display
-sims = Label(root, text="SIMS-QC", bg="#ffffff", font=("Times New Roman", 25), pady=5)
-padding_one = Label(root, text="", padx=37, bg="#ffffff")
+sims = Label(root, text="SIMS-QC", bg="#ffffff", font=("Times New Roman", 26), pady=5)
 account_icon = ImageTk.PhotoImage(Image.open("images/account.png"))
 account_img = Label(image=account_icon, bg="#ffffff")
 log_in = Label(root, text="Log In", bg="#ffffff", pady=5, font=("Arial", 25))
-padding_two = Label(root, text="", bg="#ffffff")
 teacher_id = Label(root, text="Teacher ID", bg="#ffffff", font=("Arial", 14))
 teacherid_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12))
-padding_three = Label(root, text="", bg="#ffffff")
 pass_word = Label(root, text="Password", bg="#ffffff", font=("Arial", 14))
 password_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12))
-padding_four = Label(root, text="", padx=5, bg="#ffffff")
 password_con = Label(root, text="Confirm Password", font=("Arial", 14), bg="#ffffff")
 confirm_ent = Entry(root, width=25, bg="#fafafa", font=("Times New Roman", 12))
-padding_five = Label(root, text="", bg="#ffffff", pady=10)
 submit_btn = Button(root, text="Submit", font=("Arial", 13), bg="#138808", fg="#ffffff", width=7, command=submit)
-padding_six = Label(root, text="Don't have an account yet? Then sign up.", bg="#ffffff", pady=15)
+sign_up_text = Label(root, text="Don't have an account yet? Then sign up.", bg="#ffffff", pady=10)
 sign_up = Button(root, text="Sign Up", fg="#ffffff", bg="#1338be", command=onClick)
 
 
-
 #widgets being displayed using the grid method
-sims.grid(column=3, row=1)
-padding_one.grid(column=2, row=2)
-account_img.grid(column=3, row=2)
-log_in.grid(column=3, row=3)
-padding_two.grid(column=3, row=4)
-teacher_id.grid(column=3, row=5)
-teacherid_ent.grid(column=3, row=6)
-padding_three.grid(column=3, row=7)
-pass_word.grid(column=3, row=8)
-password_ent.grid(column=3, row=9)
-padding_five.grid(column=3, row=13)
-submit_btn.grid(column=3, row=14)
-padding_six.grid(column=3, row=15)
-sign_up.grid(column=3, row=16)
+sims.grid(column=1, row=1, ipadx=120)
+account_img.grid(column=1, row=2)
+log_in.grid(column=1, row=3)
+teacher_id.grid(column=1, row=4, pady=(15,0))
+teacherid_ent.grid(column=1, row=5)
+pass_word.grid(column=1, row=6, pady=(15,0))
+password_ent.grid(column=1, row=7)
+submit_btn.grid(column=1, row=8, pady=(25,25))
+sign_up_text.grid(column=1, row=9)
+sign_up.grid(column=1, row=10)
+
 
 
 root.mainloop()
--- sims-qc_copy.py
+++ sims-qc_copy.py
@@ -12,31 +12,31 @@ root.config(bg="#ffffff")
 root.resizable(width=False, height=False)
 
 
+padding_one = Label(root, text="", padx=110, bg="#ffffff")
 logo_img = ImageTk.PhotoImage(Image.open("images/qc_logo.jpg"))
 logo = Label(image=logo_img, bg="#ffffff")
 heading = Label(root, text="Welcome to SIMS-QC 1.0.1", font=("Times New Roman", 25), bg="#ffffff", pady=10)
 create_btn = Button(root, text="Create new record", font=("Arial", 13), padx=109, pady=15)
+padding_two = Label(root, text="", font=("Arial", 3), bg="#ffffff")
 view_btn = Button(root, text="View existing records", font=("Arial", 13), padx=100, pady=15)
+padding_three = Label(root, text="", font=("Arial", 3), bg="#ffffff")
 update_btn = Button(root, text="Update records", font=("Arial", 13), padx=121, pady=15)
+padding_four = Label(root, text="", font=("Arial", 2), bg="#ffffff")
 delete_btn = Button(root, text="Delete records", font=("Arial", 13), pady=13, padx=2)
 generate_btn = Button(root, text="Report Cards", font=("Arial", 13), padx=7, pady=13)
 exit_btn = Button(root, text="Exit", font=("Arial", 13), padx=35, pady=13)
 
 
-logo.grid(column=1, row=1, ipadx=300)
-heading.grid(column=1, row=2)
-create_btn.grid(column=1, row=3, pady=(0, 7))
-view_btn.grid(column=1, row=5, pady=(0, 7))
-update_btn.grid(column=1, row=7, pady=(0, 7))
-delete_btn.grid(column=1, row=9, padx=(0,243))
-generate_btn.grid(column=1, row=9, columnspan=1, padx=(15,0))
-exit_btn.grid(column=1, row=9, columnspan=2, padx=(259,0))
 
-
-
-
-
-
-
-
-
+padding_one.grid(column=1, row=1)
+logo.grid(column=2, row=2)
+heading.grid(column=2, row=3)
+create_btn.grid(column=2, row=4)
+padding_two.grid(column=2, row=5)
+view_btn.grid(column=2, row=6)
+padding_three.grid(column=2, row=7)
+update_btn.grid(column=2, row=8)
+padding_four.grid(column=2, row=9)
+delete_btn.grid(column=2, row=10, padx=(0,243))
+generate_btn.grid(column=2, row=10, columnspan=1, padx=(15,0))
+exit_btn.grid(column=2, row=10, columnspan=2, padx=(259,0)
